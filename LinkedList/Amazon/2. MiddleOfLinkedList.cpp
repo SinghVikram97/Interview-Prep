e270d3ec4736b155522d6abc7a17bfb06baef486
SinghVikram97 <vikram.bedi97@gmail.com>
2018-06-05 18:38:17 +0530

    Create 2. MiddleOfLinkedList.cpp

---
+++ LinkedList/Amazon/2. MiddleOfLinkedList.cpp
@@ -0,0 +1,32 @@
+https://practice.geeksforgeeks.org/problems/finding-middle-element-in-a-linked-list/1
+
+https://drive.google.com/open?id=1OMcJBP3Eg7qm6DMbFPZdf93zhDaAXDX7
+https://drive.google.com/open?id=1QyhxWEC_vKBvTpVV3EzV1OEosZNeY5q0
+
+int getMiddle(Node *head)
+{
+   // Your code here
+   
+   if(head==NULL){
+       return -1;
+       
+   }
+   Node *slow=head;
+   Node *fast=head;
+   
+   while(fast->next!=NULL && fast->next->next!=NULL){
+       
+       fast=fast->next->next;
+       slow=slow->next;
+       
+   }
+   
+   // Odd elements
+   if(fast->next==NULL){
+       return slow->data;
+   }
+   // Even elements
+   else if(fast->next->next==NULL){
+       return slow->next->data;
+   }
+}
