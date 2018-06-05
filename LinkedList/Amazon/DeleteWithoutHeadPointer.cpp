098a88291923ba9116cfd164c0198f9fd8726bb7
SinghVikram97 <vikram.bedi97@gmail.com>
2018-06-05 18:08:43 +0530

    Create DeleteWithoutHeadPointer.cpp

---
+++ LinkedList/Amazon/DeleteWithoutHeadPointer.cpp
@@ -0,0 +1,17 @@
+https://drive.google.com/open?id=1Zr2jhPXxqM1j7JTIRM7xaaiHPt7mnSDP
+
+O(1)
+
+void deleteNode(Node *node)
+{
+   // Your code heret
+   
+   int swap=node->data;
+   node->data=node->next->data;
+   node->next->data=swap;
+   
+   Node *temp=node->next;
+   node->next=node->next->next;
+   delete temp;
+   
+}
