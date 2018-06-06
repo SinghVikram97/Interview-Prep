0c9de4048a0153ad44f239e50495cd6f22c5d2c0
SinghVikram97 <vikram.bedi97@gmail.com>
2018-06-06 18:03:16 +0530

    Create 6. ReverseLL(IMP).cpp

---
+++ LinkedList/Amazon/6. ReverseLL(IMP).cpp
@@ -0,0 +1,23 @@
+
+https://practice.geeksforgeeks.org/problems/reverse-a-linked-list/1
+
+https://drive.google.com/open?id=1SeNduBFT2Z0Z9OqOHxDaASpveYFPNg08
+
+Node* reverse(Node *head)
+{
+  // Your code here
+  
+  Node *prev=NULL;
+  Node *cur=head;
+  Node *next=NULL;
+  
+  while(cur!=NULL){
+      
+      next=cur->next;
+      cur->next=prev;
+      prev=cur;
+      cur=next;
+      
+  }
+  return prev;  
+}
