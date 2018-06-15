b74ab265cabc46184ed79c062a650f839d089db9
SinghVikram97 <vikram.bedi97@gmail.com>
2018-06-15 23:30:55 +0530

    Update 5.(VIMP) BoundaryTraversal and printing leaf nodes.cpp

--- Trees/Binary Tree/5.(VIMP) BoundaryTraversal and printing leaf nodes.cpp
+++ Trees/Binary Tree/5.(VIMP) BoundaryTraversal and printing leaf nodes.cpp
@@ -53,6 +53,7 @@ void printLeafNodes(Node *p){
         // Move left most
         printLeafNodes(p->left);
         
+        // CONTROL COMES HERE WHEN P->LEFT IS NULL SO IT RETURNS NULL AND WE EXECUTE LINE 58
         // Print if leaf node
         if(p->left==NULL && p->right==NULL){
             cout<<p->data<<" ";
@@ -61,76 +62,8 @@ void printLeafNodes(Node *p){
         // Return to node which is parent of leftmost leaf node and move to it's right
         printLeafNodes(p->right);
     }
-    
-}
-void printBoundary(Node *root)
-{
-    
-    if(root!=NULL){
-        
-        cout<<root->data<<" ";
-        
-        // Don't bass root to left and right boundary as it will be printed twice as it is in both boundaries
-        leftBoundary(root->left);
-        
-        printLeafNodes(root);
-        
-        rightBoundary(root->right);
-        
-    }
-     
-}void leftBoundary(Node *p){
-    
-    if(p!=NULL){
-        
-        // 1st try to move left
-        if(p->left!=NULL){
-            
-            // To ensure top down order 1st print the node and then go down left or right
-            cout<<p->data<<" ";
-            leftBoundary(p->left);
-        }
-        // If can't move right
-        else if(p->right!=NULL){
-            cout<<p->data<<" ";
-            leftBoundary(p->right);
-        }
-        
-        // Otherwise it is a leaf node (BOTH NULL it returns back)
-    }
-    
-}
-void rightBoundary(Node *p){
-    
-    if(p!=NULL){
-        
-        if(p->right!=NULL){
-            
-            // To ensure bottom up manner 1st move right and print current node at last
-            rightBoundary(p->right);
-            cout<<p->data<<" ";
-        }
-        else if(p->left!=NULL){
-            rightBoundary(p->left);
-            cout<<p->data<<" ";
-        }
-        
-        // Otherwise it is a leaf node (BOTH NULL it returns back)
-    }
-}
-void printLeafNodes(Node *p){
-    
-    
-    if(p!=NULL){
-        
-        printLeafNodes(p->left);
-        
-        // Print if leaf node
-        if(p->left==NULL && p->right==NULL){
-            cout<<p->data<<" ";
-        }
-        
-        printLeafNodes(p->right);
+    else{
+        return NULL;
     }
     
 }
