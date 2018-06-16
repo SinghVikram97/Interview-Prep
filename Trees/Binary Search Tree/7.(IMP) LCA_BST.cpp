abaea82ee01739c1c90ad4ba41ce7aec6d75b041
SinghVikram97 <vikram.bedi97@gmail.com>
2018-06-16 21:34:31 +0530

    Update 7.(IMP) LCA_BST.cpp

--- Trees/Binary Search Tree/7.(IMP) LCA_BST.cpp
+++ Trees/Binary Search Tree/7.(IMP) LCA_BST.cpp
@@ -2,6 +2,10 @@ The above functions assume that n1 and n2 both are in BST.
 If n1 and n2 are not present, then they may return incorrect result. 
 Extend the above solutions to return NULL if n1 or n2 or both not present in BST.
 
+	
+https://drive.google.com/open?id=1QY41sruigy5R1QEiLBrB_bSi7ozFwjTC
+
+
 if(search_bst(key1,head)!=NULL && search_bst(key2,head)!=NULL)
 	{
 		node *temp=find_lca(head,min(key1,key2),max(key1,key2));
