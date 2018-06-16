98dfa6d52af22562aefef08f6209f8df45fdd0fe
SinghVikram97 <vikram.bedi97@gmail.com>
2018-06-17 00:05:01 +0530

    Create 14(3).(VIMP) LCA.cpp

---
+++ Trees/Binary Tree/14(3).(VIMP) LCA.cpp
@@ -0,0 +1,17 @@
+In last code
+
+1
+4
+10 20 L 10 30 R 20 40 L 20 60 R
+20 60
+
+Gives 20 correct without checking if 60 present or not
+So it will give 20 for following too
+
+1
+4
+10 20 L 10 30 R 20 40 L 20 60 R
+20 50
+
+But 50 not present in the tree
+
