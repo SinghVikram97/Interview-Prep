8749280ea853d255ec003ae46481db0776b13d76
SinghVikram97 <vikram.bedi97@gmail.com>
2018-06-19 19:06:53 +0530

    Update 1. MaximumSumSubarray(1).cpp

--- Arrays/1. MaximumSumSubarray(1).cpp
+++ Arrays/1. MaximumSumSubarray(1).cpp
@@ -2,26 +2,18 @@ https://practice.geeksforgeeks.org/problems/kadanes-algorithm/0
 
 https://drive.google.com/open?id=1ulOcLKcLn3GTnZ_oD5rSD1nqsWu5QXNL
 
-BRUTE FORCE-O(n3)
-
-    long int SubArraySum(int arr[], int n)
-{
-    long int result = 0;
- 
-    // Pick starting point
-    for (int i=0; i <n; i++)
-    {
-        // Pick ending point
-        for (int j=i; j<n; j++)
-        {
-            // sum subarray between current
-            // starting and ending points
-            for (int k=i; k<=j; k++)
-                result += arr[k] ;
-        }
-    }
-    return result ;
-}
+BRUTE FORCE-O(n2)
+  int sum,max_sum=INT_MIN;
+  for(int i=0;i<n;i++){
+      sum=0;
+      // Explore subarrays starting from i
+      for(int j=i;j<n;j++){
+          sum=sum+v[j]
+          if(sum>max_sum){
+              max_sum=sum;
+          }
+      }
+  }
 
 #include<bits/stdc++.h>
 using namespace std;
