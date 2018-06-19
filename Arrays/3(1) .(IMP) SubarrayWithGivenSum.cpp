75917cfd16d866f33db086f58cdfee940f1be40a
SinghVikram97 <vikram.bedi97@gmail.com>
2018-06-19 19:05:14 +0530

    Update 3(1) .(IMP) SubarrayWithGivenSum.cpp

--- Arrays/3(1) .(IMP) SubarrayWithGivenSum.cpp
+++ Arrays/3(1) .(IMP) SubarrayWithGivenSum.cpp
@@ -1,7 +1,7 @@
 https://practice.geeksforgeeks.org/problems/subarray-with-given-sum/0
 
-https://drive.google.com/open?id=1y1upgWSjnRfjVE30PZ_7bQ_iMWSAFV5O O(n2)
-
+O(n2)
+    
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
@@ -10,35 +10,33 @@ int main(){
     cin>>t;
     while(t--){
     
-        int n,sum;
-        cin>>n>>sum;
-        // 1 based indexing
+        int n,target;
+        cin>>n>>target;
+        
         vector<int> v(n+1);
         for(int i=1;i<=n;i++){
             cin>>v[i];
         }
         
-        vector<int> prefix(n+1);
-        // Important
-        prefix[0]=0;
-        for(int i=1;i<=n;i++){
-            prefix[i]=prefix[i-1]+v[i];
-        }
-        
-        
-        int cs;
+        int sum;
+        int start,end;
         bool found=false;
-        int s,e;
+        
         for(int i=1;i<=n;i++){
+            
+            sum=0;
+            
+            // Explore all subarrays starting with i
             
             for(int j=i;j<=n;j++){
                 
-                // Calculate sum b/w subarray i to j
-                cs=prefix[j]-prefix[i-1];
-                if(cs==sum){
+                // Subarray from i to j
+                
+                sum=sum+v[j];
+                if(sum==target){
                     found=true;
-                    s=i;
-                    e=j;
+                    start=i;
+                    end=j;
                     break;
                 }
                 
@@ -47,11 +45,11 @@ int main(){
                 break;
             }
         }
-       if(found){
-           cout<<s<<" "<<e<<endl;
-       }
-       else{
-           cout<<-1<<endl;
-       }
+        if(found){
+            cout<<start<<" "<<end<<endl;
+        }       
+        else{
+            cout<<-1<<endl;
+        }
     }
 }
