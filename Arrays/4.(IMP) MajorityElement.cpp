24b4f9b1ed5f0d75f8402035be7e3ee3cd794497
SinghVikram97 <vikram.bedi97@gmail.com>
2018-06-20 12:01:16 +0530

    Create 4.(IMP) MajorityElement.cpp

---
+++ Arrays/4.(IMP) MajorityElement.cpp
@@ -0,0 +1,59 @@
+https://practice.geeksforgeeks.org/problems/majority-element/0
+
+https://drive.google.com/open?id=19ggCx87-FAsSBb12ZOm_VCeIsWx794kA
+https://drive.google.com/open?id=1XYXl4fH26Z9RNGI44S8flWKZ78Z-zC7w
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+    
+        int n;
+        cin>>n;
+        vector<int> v(n);
+        for(int i=0;i<n;i++){
+            cin>>v[i];
+        }
+        
+        // Select 1st element as majority element
+        int majority_index=0;
+        int count=1;
+        
+        for(int i=1;i<n;i++){
+            
+            // If same then increment it's count
+            if(v[i]==v[majority_index]){
+                count++;
+            }
+            // If different cancel it's count
+            else{
+                count--;
+            }
+            
+            // If count reduces to zero select current element as majority element and set count to 1
+            if(count==0){
+                majority_index=i;
+                count=1;
+            }
+            
+        }
+        
+        // It returns majority element candidate so check if it's count>n/2
+        count=0;
+        for(int i=0;i<n;i++){
+            if(v[i]==v[majority_index]){
+                count++;
+            }
+        }
+        if(count>n/2){
+            cout<<v[majority_index]<<endl;
+        }
+        else{
+            cout<<"NO Majority Element"<<endl;
+        }
+        
+    }
+}
