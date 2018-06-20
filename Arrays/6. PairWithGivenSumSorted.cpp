9a93154adfdf6ed1fb6376701367f6e51ad374c9
SinghVikram97 <vikram.bedi97@gmail.com>
2018-06-20 12:28:18 +0530

    Create 6. PairWithGivenSumSorted.cpp

---
+++ Arrays/6. PairWithGivenSumSorted.cpp
@@ -0,0 +1,23 @@
+
+bool findPair(vector<int> &v,int n,int k){
+    
+    sort(v.begin(),v.end());
+    
+    // Start and end
+    int l=0,r=n-1;
+       
+    while(l<=r){
+        
+        if(v[l]+v[r]==k){
+            return true;
+        }
+        else if(v[l]+v[r]<k){
+            l++;
+        }
+        else{
+            r--;
+        }
+    }
+    
+    return false;
+}
