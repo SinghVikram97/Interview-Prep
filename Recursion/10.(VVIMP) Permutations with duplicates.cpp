c90289a19faccbae44b649922d0002f7cc2a6097
SinghVikram97 <vikram.bedi97@gmail.com>
2018-06-22 22:54:21 +0530

    Create 10.(VVIMP) Permutations with duplicates.cpp

---
+++ Recursion/10.(VVIMP) Permutations with duplicates.cpp
@@ -0,0 +1,50 @@
+https://leetcode.com/problems/permutations-ii/description/
+
+https://drive.google.com/open?id=1JOErCkCCJDq1ZuFsfEw9xuvHmIbC0SA0
+https://drive.google.com/open?id=1I57bNCi1quNz0F9yZ_MSAZS4zi5mkfyU
+https://drive.google.com/open?id=1Cvv5R_QpBSD0vpSUZyLHjw6G7_zBptV6
+
+class Solution {
+public:
+    void backtrack(vector<int> &nums,int start,vector<vector<int> > &finalAns){
+        
+        if(start==nums.size()){
+            finalAns.push_back(nums);
+            return;
+        }
+        
+        unordered_set<int> s;
+        for(int i=start;i<nums.size();i++){
+            
+            
+            if(s.find(nums[i])!=s.end()){
+                continue;
+            }
+            
+            s.insert(nums[i]);
+            
+            // Fix every number at start
+            swap(nums[i],nums[start]);
+            
+            // Now find permutation of remaining elements ie [1,2,3] fix 3 at start [3,2,1] now find permutations of [2,1]
+            backtrack(nums,start+1,finalAns);  // Carefull not i+1 it is start+1
+            
+            // Backtrack
+            swap(nums[i],nums[start]);
+            
+            
+        }
+        
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        
+        // Duplicates so 1st sort
+        sort(nums.begin(),nums.end());
+        
+        vector<vector<int> > finalAns;
+        
+        backtrack(nums,0,finalAns);
+        
+        return finalAns;
+    }
+};
