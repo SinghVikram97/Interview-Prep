908e4d34f4ebd0c0bc1395ec3299cbce38279ef8
SinghVikram97 <vikram.bedi97@gmail.com>
2018-06-24 00:02:04 +0530

    Create 9.(IMP) Min ways to make change.cpp

---
+++ Dynamic Programming/9.(IMP) Min ways to make change.cpp
@@ -0,0 +1,29 @@
+https://leetcode.com/problems/coin-change/description/
+
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        
+        int n=coins.size();
+        long long int dp[amount+1];
+        dp[0]=0;  // Notice we have taken 0 here not 1 as opposed to previous problem
+        
+        for(int i=1;i<=amount;i++){
+            
+            dp[i]=INT_MAX;
+            
+            for(int j=1;j<=n;j++){
+                
+                if(i-coins[j-1]>=0){
+                    dp[i]=min(dp[i],1+dp[i-coins[j-1]]);
+                } 
+            }
+            
+        }
+        if(dp[amount]==INT_MAX){
+            return -1;
+        }
+        return dp[amount];
+    }
+};
