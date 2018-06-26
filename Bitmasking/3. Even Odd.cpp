29798470ca6c16f01966deea583fb98b7bf53230
SinghVikram97 <vikram.bedi97@gmail.com>
2018-06-26 13:11:39 +0530

    Create 3. Even Odd.cpp

---
+++ Bitmasking/3. Even Odd.cpp
@@ -0,0 +1,20 @@
+https://drive.google.com/open?id=1q86g1_vLynLySvFxls0lqlutHI5P3LNo
+
+If last bit is 1 then no is odd else even
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n&1)
+    {
+        cout<<"Odd"<<endl;
+    }
+    else
+    {
+        cout<<"Even"<<endl;
+    }
+    
+}
