d4a8763f404d797e60d0b9d12088bd50a156c7d8
SinghVikram97 <vikram.bedi97@gmail.com>
2018-06-26 13:36:40 +0530

    Create 4. Count Set Bits.cpp

---
+++ Bitmasking/4. Count Set Bits.cpp
@@ -0,0 +1,34 @@
+https://practice.geeksforgeeks.org/problems/set-bits/0
+
+https://drive.google.com/open?id=1TMYKzlAIjCtyuwhi9jnjEyFNOSBfiotV
+
+// O(no of bits) ie sizeof(int)
+int setBitsCount1(int n){
+    
+    int count=0;
+    
+    while(n!=0){
+        
+        if(n&1){
+            count++;
+        }
+        
+        n=n>>1;
+        
+    }
+
+    return count;
+    
+}
+// O(no of set bits)
+int setBitsCount2(int n){
+    
+    int count=0;
+    
+    /// Removes last set bit in each iteration
+    while(n){
+        count++;
+        n=n&(n-1);
+    }
+    return count;
+}
