6344d15f24c9be816477c4e755ea2c442a4d8239
SinghVikram97 <vikram.bedi97@gmail.com>
2018-06-27 13:44:12 +0530

    Update 3. Square Root (Floor).cpp

--- Binary Search/3. Square Root (Floor).cpp
+++ Binary Search/3. Square Root (Floor).cpp
@@ -1,8 +1,29 @@
 https://leetcode.com/problems/sqrtx/description/
 
 Question to ask--> We need floor or ceil of non perfect squares?
-
+        
 https://drive.google.com/open?id=1gZ_UV-sOVtp42JEE7CjTE959cnI6QDLm
+
+BRUTE FORCE---->
+int floorSqrt(int x)
+{
+    // Base cases
+    if (x == 0 || x == 1)
+    return x;
+ 
+    // Staring from 1, try all numbers until
+    // i*i is greater than or equal to x.
+    int i = 1, result = 1;
+    while (result <= x)
+    {
+      i++;
+      result = i * i;
+    }
+    return i - 1;
+}
+
+.....................
+Binary Search
 
 Time Complexity=O(Log x)
 ll mySqrt(ll x) {
