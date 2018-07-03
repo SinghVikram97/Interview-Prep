7482fd880c21ad45e05f6fa943e6c5e68cf358be
SinghVikram97 <vikram.bedi97@gmail.com>
2018-07-03 13:46:11 +0530

    Create A_15 Next greater element.cpp

---
+++ IMP Questions/A_15 Next greater element.cpp
@@ -0,0 +1,14 @@
+https://practice.geeksforgeeks.org/problems/greater-on-right-side/0
+
+int replace(vector<int> &v,int n){
+    
+    // Start from right and keep track of max til now
+    int maxi=-1;
+    int temp;
+    for(int i=n-1;i>=0;i--){
+        
+        temp=v[i];
+        v[i]=maxi;
+        maxi=max(temp,maxi);
+    }
+}
