567e836d2bbb3ce83b87ecbbd9f5eee0fb65e916
SinghVikram97 <vikram.bedi97@gmail.com>
2018-07-04 17:29:53 +0530

    Update and rename A_23 Rearrange-> Move all non zero elements to front.cpp to A_23 (IMP) Rearrange-> Move all non zero elements to front.cpp

--- IMP Questions/A_23 (IMP) Rearrange-> Move all non zero elements to front.cpp
+++ IMP Questions/A_23 (IMP) Rearrange-> Move all non zero elements to front.cpp
@@ -26,3 +26,27 @@ public:
         }
     }
 };
+
+
+// Another solution
+void moveZeroes(vector<int>& nums) {
+        
+        int n=nums.size();
+        
+        int lastFoundNonZero=-1;
+        
+        // Bring all non zero elements to the front
+        // Keeping order same
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                lastFoundNonZero++;
+                // nums[lastFoundNonZero]=nums[i];
+                swap(nums[i],nums[lastFoundNonZero]);
+            }
+        }
+        
+        // // Now fill all zeroes from lastFoundNonZero+1 to n
+        // for(int i=lastFoundNonZero+1;i<n;i++){
+        //     nums[i]=0;
+        // }
+    }
