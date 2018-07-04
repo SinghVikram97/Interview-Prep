b9299a56b01364bc27f7d22cc311e2f593e2fc0c
SinghVikram97 <vikram.bedi97@gmail.com>
2018-07-04 15:37:53 +0530

    Update A_20 Root To Leaf Paths.cpp

--- IMP Questions/A_20 Root To Leaf Paths.cpp
+++ IMP Questions/A_20 Root To Leaf Paths.cpp
@@ -36,3 +36,36 @@ void printPaths(Node* root)
     printPathsUtil(root,path);
     cout<<endl;
 }
+
+........................................................
+    IMP
+// Here we are passing by reference and safing space
+// If we pass by value at each recursive call a new a vector<int> is created and a lot of space wasted
+void printPathsUtil(Node *root,vector<int> &path){
+    
+    // If null don't print anything
+    if(root==NULL){
+        return;
+    }
+    
+    // If leaf node print the path that lead to here
+    if(root->left==NULL && root->right==NULL){
+        
+        path.push_back(root->data);
+        for(int i=0;i<path.size();i++){
+            cout<<path[i]<<" ";
+        }
+        cout<<"#";
+        // Backtrack
+        path.pop_back();
+        return;
+        
+    }
+    // Otherwise add cur node data
+    path.push_back(root->data);
+    // And move to both subtree's for leaf node
+    printPathsUtil(root->left,path);
+    printPathsUtil(root->right,path);
+    // Backtrack
+    path.pop_back();
+}
