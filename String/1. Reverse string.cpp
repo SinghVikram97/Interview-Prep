4af490fb1a23f83616abe5921e66edf68b9f2377
SinghVikram97 <vikram.bedi97@gmail.com>
2018-07-08 12:57:04 +0530

    Create 1. Reverse string.cpp

---
+++ String/1. Reverse string.cpp
@@ -0,0 +1,11 @@
+string reverseString(string s){
+    int start=0;
+    int end=s.length()-1;
+    /// No harm in putting = as character swapped with same character
+    while(start<=end){
+        swap(s[start],s[end]);
+        start++;
+        end--;
+    }
+    return s;
+}
