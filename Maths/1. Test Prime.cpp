ab406d873808a34cbe3549fa3a2d3d534afb4392
SinghVikram97 <vikram.bedi97@gmail.com>
2018-07-12 20:07:35 +0530

    Update 1. Test Prime.cpp

--- Maths/1. Test Prime.cpp
+++ Maths/1. Test Prime.cpp
@@ -1,5 +1,7 @@
 https://practice.geeksforgeeks.org/problems/prime-number/0
 
+https://drive.google.com/open?id=1lEjgcvaY8VIN-zd3vVBSeiFVpq7lGAJb
+
 bool isPrime(int n){
     if(n==0 || n==1){
         return false;
@@ -10,7 +12,12 @@ bool isPrime(int n){
     if(n%2==0){
         return false;
     }
-    for(int i=2;i*i<=n;i++){
+    // Check only till under root n
+    // as if n%a=0
+    // it means n=a*b ie. both a and b divide n
+    // now one of them will be smaller than n and one should be greater than n
+    
+    for(int i=3;i*i<=n;i=i+2){
         if(n%i==0){
             return false;
         }
