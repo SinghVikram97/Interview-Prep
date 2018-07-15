d36cbaeddf0ac0a56e95ef33638e308bd6c44345
SinghVikram97 <vikram.bedi97@gmail.com>
2018-07-15 17:19:06 +0530

    Update 6.(IMP) Longest Increasing Subsequence.cpp

--- Dynamic Programming/6.(IMP) Longest Increasing Subsequence.cpp
+++ Dynamic Programming/6.(IMP) Longest Increasing Subsequence.cpp
@@ -2,6 +2,38 @@ https://leetcode.com/problems/longest-increasing-subsequence/description/
 
 Brute force generate all subsequences --- O(2^n) see leetcode solution for brute force
 
+Recursive solution-->
+class Solution {
+public:
+    void backtrack(vector<int> &nums,int start,int prev,int length,int &maxLength){
+        
+       
+        if(length>maxLength){
+            maxLength=length;
+        }
+        
+        for(int i=start;i<nums.size();i++){
+            
+            if(start==0){
+                backtrack(nums,i+1,nums[i],length+1,maxLength);
+            }   
+            else{
+                if(nums[i]>prev){
+                    backtrack(nums,i+1,nums[i],length+1,maxLength);
+                }
+            }            
+        }               
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int maxLength=-1;
+        backtrack(nums,0,-1,0,maxLength);
+        return maxLength;
+        
+    }
+};
+
+DP-->
+    
 https://drive.google.com/open?id=1xw5nK1Oq089UJm5EPwMaZ1CO79Iub3eH
 https://drive.google.com/open?id=1MUYqnzga4jhCj5JGxid7Lb_e8K8f4JD0
 https://drive.google.com/open?id=1suB4Rklme21yppYF5s65z6KnXHUC6dAh
