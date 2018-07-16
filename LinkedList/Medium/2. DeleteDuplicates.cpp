c8bd98020c33b42bad22ff00e256e084995fd66e
SinghVikram97 <vikram.bedi97@gmail.com>
2018-07-16 14:00:30 +0530

    Update 2. DeleteDuplicates.cpp

--- LinkedList/Medium/2. DeleteDuplicates.cpp
+++ LinkedList/Medium/2. DeleteDuplicates.cpp
@@ -3,28 +3,62 @@ https://practice.geeksforgeeks.org/problems/remove-duplicate-element-from-sorted
 https://drive.google.com/open?id=10m4QaToMx_6SskJ9Z5FStl7tBYJlyGnb
 https://drive.google.com/open?id=13PTZEYZaI6143NQgL1wSsn-cvOahrraO
 
-Node *removeDuplicates(Node *root)
+SORTED -> 11 11 11 21 43 43
+Node *removeDuplicates(Node *head)
 {
- // your code goes here
-    if(root==NULL){
-        return root;
+    Node *prev=NULL;
+    Node *cur=head;
+    while(cur!=NULL){
+        
+        if(prev==NULL){
+            prev=cur;
+            cur=cur->next;
+        }
+        else{
+            if(prev->data==cur->data){
+                Node *temp=cur;
+                prev->next=cur->next;
+                cur=cur->next;
+                delete temp;
+            }
+            else{
+                prev=cur;
+                cur=cur->next;
+            }
+        }
     }
- 
- 
-   Node *cur=root;
-   
-   while(cur!=NULL && cur->next!=NULL){
-       
-       if(cur->next->data==cur->data){
-           Node *temp=cur->next;
-           cur->next=cur->next->next;
-           delete temp;
-       }
-       else{
-           cur=cur->next;
-       }
-   }
-   return root;
- 
- 
+    return head;
+}
+
+UNSORTED 11 21 43 11 21 
+https://practice.geeksforgeeks.org/problems/remove-duplicates-from-an-unsorted-linked-list/1/?ref=self
+USE A SET TO FIND IF A VALUE SEEN BEFORE
+
+Node *removeDuplicates(Node *head)
+{
+    Node *prev=NULL;
+    Node *cur=head;
+    unordered_set<int> s;
+    while(cur!=NULL){
+        
+        if(prev==NULL){
+            prev=cur;
+            s.insert(cur->data);
+            cur=cur->next;
+        }
+        else{
+            if(s.find(cur->data)!=s.end()){
+                Node *temp=cur;
+                prev->next=cur->next;
+                cur=cur->next;
+                delete temp;
+            }
+            else{
+                prev=cur;
+                s.insert(cur->data);
+                cur=cur->next;
+            }
+        }
+    }
+    return head;
 }
