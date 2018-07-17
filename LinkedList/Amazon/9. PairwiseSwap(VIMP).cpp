d70acfa30f5e3bb3b86772f0547125067fdef4e5
SinghVikram97 <vikram.bedi97@gmail.com>
2018-07-17 16:22:44 +0530

    Update and rename 9. PairwiseSwap(IMP).cpp to 9. PairwiseSwap(VIMP).cpp

---
+++ LinkedList/Amazon/9. PairwiseSwap(VIMP).cpp
@@ -0,0 +1,58 @@
+https://leetcode.com/problems/swap-nodes-in-pairs/description/
+
+
+
+RECURSIVE SOLUTION--> USES SPACE DUE TO RECURSION https://drive.google.com/open?id=1l74yTuBOnIajBSd1wBCQN6EuU6ORXEPf
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        
+        ListNode *rightPart=swapPairs(head->next->next);
+        ListNode *newHead;
+        
+        head->next->next=head;
+        
+        newHead=head->next;
+        
+        head->next=rightPart;
+        
+        return newHead;
+        
+    }
+};
+
+WITHOUT RECURSION https://drive.google.com/open?id=1VW9zHftnpBcB1ZsmCyuLv9mcacwWTuCU
+
+ListNode* swapPairs(ListNode* head) {
+        
+        if(head==NULL || head->next==NULL){
+            return head;
+        }
+        
+        ListNode *prev=NULL,*cur=head,*next,*newHead;
+        
+        while(cur!=NULL && cur->next!=NULL){
+            next=cur->next->next;
+            cur->next->next=cur;
+            if(prev!=NULL){
+                prev->next=cur->next;
+            }
+            else{
+                newHead=cur->next;
+            }
+            cur->next=NULL;
+            prev=cur;
+            cur=next;
+        }
+        // Odd elements last would be left out
+        if(cur!=NULL){
+            prev->next=cur;
+        }
+        return newHead;
+    }
+
