bc97fd4f51eceeb3a1cc81a8384390e60b4f35f0
SinghVikram97 <vikram.bedi97@gmail.com>
2018-07-17 15:22:34 +0530

    Update 8. PalindromicLL (IMP).cpp

--- LinkedList/Amazon/8. PalindromicLL (IMP).cpp
+++ LinkedList/Amazon/8. PalindromicLL (IMP).cpp
@@ -1,35 +1,29 @@
+https://practice.geeksforgeeks.org/problems/check-if-linked-list-is-pallindrome/1
+   
+https://drive.google.com/open?id=1aopFhpFhs_GRqcR270cBIWfhLw1MoDJo
+https://drive.google.com/open?id=15IQKsLUeKoT79s1geoeE5Jno2qABP8HL
+https://drive.google.com/open?id=1q6VMussniKespj5VXI31vS8kjYM9PwyR
+
+/// 1->2->3->3->2->1 becomes 1->2->3->3-<-2-<-1 (Even nodes) (Mid is 2nd 3)
+/// 1->2->3->2->1 becomes 1->2->3<-2-<-1 (Even nodes) (Mid is 3)
+/// So after reversing start from beg of both list's and check till head1 doesn't become equal to mid
+
 bool isPalindrome(Node *head)
 {
-    //Your code here
-    
-    // Calculate length of linked list
-    Node *temp=head;
-    int length=0;
-    while(temp!=NULL){
-        temp=temp->next;
-        length++;
-    }
-    
-    // Find mid of linked list
-    Node *slow=head;
+    /// 1. find the mid element
     Node *fast=head;
-    while(fast->next!=NULL && fast->next->next!=NULL){
+    Node *slow=head;
+    while(fast!=NULL && fast->next!=NULL){
         fast=fast->next->next;
         slow=slow->next;
     }
     
-   https://practice.geeksforgeeks.org/problems/check-if-linked-list-is-pallindrome/1
-   
-   https://drive.google.com/open?id=1aopFhpFhs_GRqcR270cBIWfhLw1MoDJo
-   https://drive.google.com/open?id=15IQKsLUeKoT79s1geoeE5Jno2qABP8HL
-   https://drive.google.com/open?id=1q6VMussniKespj5VXI31vS8kjYM9PwyR
-   
-   // Increment mid by one (In even we want 2nd mid element and In odd we don't want mid)
-    slow=slow->next;
+    // 2. So the mid is slow
+    Node *mid=slow;
     
-    // Reverse the linked list starting from this slow pointer
-    Node *cur=slow;
+    // 3. Now reverse the linked list starting from mid
     Node *prev=NULL;
+    Node *cur=mid;
     Node *next=NULL;
     
     while(cur!=NULL){
@@ -38,28 +32,22 @@ bool isPalindrome(Node *head)
         cur->next=prev;
         prev=cur;
         cur=next;
-        
     }
     
+    // 4. Compare both linked lists starting from both their heads
     Node *head1=head;
-    Node *head2=prev; //(Cur becomes NULL)
+    Node *head2=prev; 
     
-    // ex. 1->2->3->3<-2<-1 (Even)
-    // ex. 1->2->3->2<-1 (Odd)
-    
-    // Check if they are equal
-    length=length/2;
-    
-    while(length>0){
+    // Mid indicates start of 2nd portion so head!=mid
+    while(head1!=mid){
         if(head1->data!=head2->data){
             return false;
         }
-        else{
-            head1=head1->next;
-            head2=head2->next;
-        }
-        length=length-1;
+        head1=head1->next;
+        head2=head2->next;
     }
     return true;
-    
 }
+
+   
+
