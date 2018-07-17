62bc3dc362b8c69a6ae09dda042cb70c1edebf8c
SinghVikram97 <vikram.bedi97@gmail.com>
2018-07-17 19:33:33 +0530

    Update and rename 19. NthNodeFromLast.cpp to A_19(IMP). NthNodeFromLast.cpp

--- LinkedList/Amazon/A_19(IMP). NthNodeFromLast.cpp
+++ LinkedList/Amazon/A_19(IMP). NthNodeFromLast.cpp
@@ -29,3 +29,8 @@ int getNthFromLast(Node *head, int n)
            return temp->data;
        }
 }
+
+Maintain two pointers – reference pointer and main pointer. 
+Initialize both reference and main pointers to head. 
+First move reference pointer to n nodes from head.
+Now move both pointers one by one until reference pointer reaches end. Now main pointer will point to nth node from the end. Return main pointer.
