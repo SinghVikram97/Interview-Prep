bf273a255eca52bac974c5ce0d0d798bc125a26a
SinghVikram97 <vikram.bedi97@gmail.com>
2024-04-18 18:38:57 -0400

    Update 9.(IMP) First non repeating character in a stream.cpp

--- Arrays/9.(IMP) First non repeating character in a stream.cpp
+++ Arrays/9.(IMP) First non repeating character in a stream.cpp
@@ -2,68 +2,36 @@ https://practice.geeksforgeeks.org/problems/first-non-repeating-character-in-a-s
 
 https://drive.google.com/open?id=1hd2xVMX_lm8xXh94rK2INoP7BhXpMtPV
 
-Time complexity???
+class Solution {
+	public:
+		string FirstNonRepeating(string A){
+		    vector<int> freq(26);
+		    int n = A.length();
+		    
+		    string ans="";
+		    
+		    queue<char> mq;
+		    
+		    for(int i=0;i<n;i++){
+		        freq[A[i]-'a']++;
+		        
+		        if(freq[A[i]-'a']==1){
+		            mq.push(A[i]);
+		        }
+		        
+		        while(!mq.empty() && freq[mq.front()-'a']>1){
+		            mq.pop();
+		        }
+		        
+		        if(!mq.empty()){
+		            ans+=mq.front();
+		        }else{
+		            ans+="#";
+		        }
+		    }
+		    
+		    return ans;
+		    
+		}
 
-#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-
-    int t;
-    cin>>t;
-    while(t--){
-        
-        int n;
-        cin>>n;
-        
-        vector<char> ch(n);
-        
-        for(int i=0;i<n;i++){
-            cin>>ch[i];
-        }
-        
-        int freq[26];
-        
-        for(int i=0;i<26;i++){
-            
-            freq[i]=0;
-        }
-        
-        queue<char> q;
-        
-        for(int i=0;i<n;i++){
-            
-            // Increase frequency of current element
-            freq[ch[i]-'a']++;
-            
-            // Push it into the queue only if freq=1
-            if(freq[ch[i]-'a']==1){
-                q.push(ch[i]);
-            }
-            
-            
-            while(!q.empty()){
-                
-                // Try element in front of the queue
-                
-                // If it's frequency>1 it can't be non repeating pop it out and try next one
-                if(freq[q.front()-'a']>1){
-                    q.pop();
-                }
-                // Freq=1 so print and break
-                else{
-                    cout<<q.front()<<" ";
-                    break;
-                }
-                
-            }
-            // Can't find any non repeating character
-            if(q.empty()){
-                cout<<-1<<" ";
-            }
-            
-        }
-        cout<<endl;
-    }
-	
-}
+};
