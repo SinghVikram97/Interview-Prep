00cc8d2c9d71a8c51189f177327eb3fa5acfe9c4
SinghVikram97 <vikram.bedi97@gmail.com>
2024-04-18 15:03:20 -0400

    Update 3(1) .(IMP) SubarrayWithGivenSum.cpp

--- Arrays/3(1) .(IMP) SubarrayWithGivenSum.cpp
+++ Arrays/3(1) .(IMP) SubarrayWithGivenSum.cpp
@@ -53,3 +53,36 @@ int main(){
         }
     }
 }
+
+// https://leetcode.com/problems/subarray-sum-equals-k/description/
+// O(N)
+// [a,b,c,d,e]
+
+// SubarraySum(c...d)=CurrentSum(a...d)-previousSubrraySum(a..b)
+
+// TargetSum = CurrentSum - previousSubarraySum
+
+// previouSubarraySum = CurrentSum - targetSum
+
+// So we store frequency of how many times we have seen this previousSubarraySum
+
+// Add (0,1) for full subarray
+
+// ie. [1,2,3],,, 6-0(empty array)
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        // freq map to store freq of currentSum encountered till now
+        unordered_map<int,int> myMap;
+        myMap[0]=1;
+
+        int currentSum=0;
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            currentSum+=nums[i];
+            ans+=myMap[currentSum-k];
+            myMap[currentSum]++;
+        }
+        return ans;
+    }
+};
